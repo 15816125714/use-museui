a5e74b1c34267ca44a9ed5ea3763bcb158eb1ff6
zhongyh <zhongyh@la-inc.cn>
2021-02-08 00:25:32 +0800

    feat: dialog使用action-btn

--- src/modules/dialog/view/index.vue
+++ src/modules/dialog/view/index.vue
@@ -7,8 +7,9 @@
     <mu-action-btn>
       <mu-button class="one-px" large>default</mu-button>
     </mu-action-btn>
-    <mu-dialog width="400px" :open.sync="visible" :actionBtn="true">
-      <mu-action-btn @cancel="visible = !visible"></mu-action-btn>
+    <mu-dialog width="400px" :open.sync="visible" needActionBtn @confirm="handleConfirm">
+      <mu-text-field v-model="username" label="用户名" full-width></mu-text-field>
+      <!-- <mu-action-btn @cancel="visible = !visible"></mu-action-btn> -->
     </mu-dialog>
   </div>
 </template>
@@ -18,9 +19,14 @@ export default {
   data() {
     return {
       visible: false,
+      username: "",
     }
   },
   methods: {
+    handleConfirm() {
+      console.log("confirm --------------- ");
+      this.visible = false;
+    },
   }
 }
 </script>
