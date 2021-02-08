e3a45cfb8455592dda3c96cd8038bcbb07f5691b
zhongyanghua_vendor <zhongyanghua_vendor@sensetime.com>
2021-02-08 18:36:08 +0800

    feat: drawer示例

---
+++ src/modules/drawer/route.js
@@ -0,0 +1,4 @@
+export default {
+  path: "/drawer",
+  component: () => import("./view/index"),
+}
---
+++ src/modules/drawer/view/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demp-page">
+    <mu-button
+      @click="visible = !visible"
+      color="primary"
+    >Drawer</mu-button>
+    <mu-action-btn>
+      <mu-button class="one-px" large>default</mu-button>
+    </mu-action-btn>
+    <mu-drawer :open.sync="visible" needActionBtn @confirm="handleConfirm">
+    </mu-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      username: "",
+    }
+  },
+  methods: {
+    handleConfirm() {
+      console.log("confirm --------------- ");
+      this.visible = false;
+    },
+  }
+}
+</script>
